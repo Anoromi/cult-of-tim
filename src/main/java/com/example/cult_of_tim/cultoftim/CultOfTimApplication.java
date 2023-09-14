c6c19ee64bbe2d8a76cc2066f816762c1aa3183f
Andrii Zahorulko <72703146+Anoromi@users.noreply.github.com>
2023-09-14 17:58:53 +0300

    Initial commit

---
+++ src/main/java/com/example/cult_of_tim/cultoftim/CultOfTimApplication.java
@@ -0,0 +1,13 @@
+package com.example.cult_of_tim.cultoftim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CultOfTimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CultOfTimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/cult_of_tim/cultoftim/CultOfTimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cult_of_tim.cultoftim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CultOfTimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
