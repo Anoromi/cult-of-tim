7000aa34e4692295b0cb9292332c07b8a7c49941
OleksandrSeverhin <alexsevergin92@gmail.com>
2023-09-20 16:13:39 +0300

    Added getters for an email and a password in User class, created CreateUserHandler class

---
+++ src/main/java/com/example/cult_of_tim/cultoftim/handlers/CreateUserHandler.java
@@ -0,0 +1,14 @@
+package com.example.cult_of_tim.cultoftim.handlers;
+
+import com.example.cult_of_tim.cultoftim.models.User;
+
+/**
+ * This class validates User emails and
+ * @author Oleksndr Severhin
+ */
+public class CreateUserHandler {
+
+    public void checkUser(User user) {
+        String email = user.getEmail();
+    }
+}
