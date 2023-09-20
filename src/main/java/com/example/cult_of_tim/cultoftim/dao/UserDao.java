eaca36af7f78856178f947966e504f60e01baccb
Andrii Zahorulko <72703146+Anoromi@users.noreply.github.com>
2023-09-20 10:21:03 +0300

    add exceptions for UserDao methods

--- src/main/java/com/example/cult_of_tim/cultoftim/dao/UserDao.java
+++ src/main/java/com/example/cult_of_tim/cultoftim/dao/UserDao.java
@@ -2,13 +2,15 @@ package com.example.cult_of_tim.cultoftim.dao;
 
 import com.example.cult_of_tim.cultoftim.models.User;
 
+import java.util.Optional;
+
 public interface UserDao {
 
-    User getUser(String token);
+    Optional<User> getUser(String token);
 
-    void createAccount(String email, String encryptedPassword);
+    void createAccount(String email, String encryptedPassword) throws IllegalArgumentException;
 
-    void login(String email, String encryptedPassword);
+    String login(String email, String encryptedPassword) throws IllegalArgumentException;
 
     void deleteAccount(String email);
 
