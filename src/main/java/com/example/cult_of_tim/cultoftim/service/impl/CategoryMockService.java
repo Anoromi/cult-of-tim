648b623772add4ad50b9cd71ebb98aa2623c08d3
Oleg Zagryvyi <olegzagraf@gmail.com>
2023-09-21 16:10:09 +0300

    category service mock

---
+++ src/main/java/com/example/cult_of_tim/cultoftim/service/impl/CategoryMockService.java
@@ -0,0 +1,59 @@
+package com.example.cult_of_tim.cultoftim.service.impl;
+
+import com.example.cult_of_tim.cultoftim.dao.CategoryDao;
+import com.example.cult_of_tim.cultoftim.models.Category;
+import com.example.cult_of_tim.cultoftim.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CategoryMockService implements CategoryService {
+
+    private /*final*/ CategoryDao categoryDao;
+
+    // better
+    /*
+    @Autowired
+    public CategoryMockService(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }*/
+
+    @Autowired
+    public void setCategoryDao(CategoryDao categoryDao) {
+        this.categoryDao = categoryDao;
+    }
+
+    @Override
+    public Optional<Category> getCategoryById(Long id) {
+        return categoryDao.getCategoryById(id);
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return categoryDao.getAllCategories();
+    }
+
+    @Override
+    public Long createCategory(String name) {
+        Category newCategory = new Category();
+        newCategory.setName(name);
+
+        categoryDao.createCategory(newCategory);
+
+        return newCategory.getId();
+    }
+
+    @Override
+    public void updateCategory(Long id, Category updatedCategory) {
+        updatedCategory.setId(id);
+        categoryDao.updateCategory(updatedCategory);
+    }
+
+    @Override
+    public void deleteCategory(Long id) {
+        categoryDao.deleteCategoryById(id);
+    }
+}
