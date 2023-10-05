c11cac95810a32589f3d8ede361ba3018dec210e
Andrii Zahorulko <72703146+Anoromi@users.noreply.github.com>
2023-10-05 12:12:40 +0300

    Remove useless functions in BookRepository

--- src/main/java/com/example/cult_of_tim/cultoftim/repositories/BookRepository.java
+++ src/main/java/com/example/cult_of_tim/cultoftim/repositories/BookRepository.java
@@ -11,17 +11,10 @@ import java.util.Optional;
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 
-    Optional<Book> findById(Long id);
-
-    List<Book> findAll();
-
     List<Book> findByAuthorId(Long authorId);
 
     List<Book> findByCategoryId(Long categoryId);
 
     List<Book> findByAvailable(boolean available);
 
-    Book save(Book book);
-
-    void deleteById(Long id);
 }
