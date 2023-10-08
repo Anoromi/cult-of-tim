054ebd32e738f900d08c13afabcf1f915e1ffa58
Oleg Zagryvyi <olegzagraf@gmail.com>
2023-10-08 11:52:27 +0300

    remake dto with lombok

--- src/main/java/com/example/cult_of_tim/cultoftim/dto/PromotionDto.java
+++ src/main/java/com/example/cult_of_tim/cultoftim/dto/PromotionDto.java
@@ -1,52 +1,21 @@
 package com.example.cult_of_tim.cultoftim.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class PromotionDto {
     private Long id;
     private String description;
     private LocalDateTime startDate;
     private LocalDateTime endDate;
     private boolean globalPromotion;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
-
-    public LocalDateTime getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDateTime startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDateTime getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDateTime endDate) {
-        this.endDate = endDate;
-    }
-
-    public boolean isGlobalPromotion() {
-        return globalPromotion;
-    }
-
-    public void setGlobalPromotion(boolean globalPromotion) {
-        this.globalPromotion = globalPromotion;
-    }
 }
 
